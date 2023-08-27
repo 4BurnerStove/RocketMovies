442911d9174e4f1c7e50492b082d1267c29bc6d3
Andre Morryson <amorrysson77@gmail.com>
2023-08-27 12:07:49 -0300

    Create Hook auth

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,27 @@
+import { useAuth } from '../../hooks/auth'
+import { useState } from 'react'
+
 import { Input } from '../../Components/Input'
 import { Button } from '../../Components/Button'
 import { ButtonText } from '../../Components/ButtonText'
+
+
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 import { Container, Form, Background } from './styles'
 
 export function SignIn() {
-  return( 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
+  return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
@@ -15,11 +29,21 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input type='text' placeholder='E-mail' icon={FiMail}></Input>
-        <Input type='password'placeholder='Senha'icon={FiLock}></Input>
-        <Button name='Entrar'></Button>
+        <Input
+          type='text'
+          placeholder='E-mail'
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        ></Input>
+        <Input
+          type='password'
+          placeholder='Senha'
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        ></Input>
+        <Button name='Entrar' onClick={handleSignIn}></Button>
         <Link to='/signup'>
-        <ButtonText name='Criar conta'></ButtonText>
+          <ButtonText name='Criar conta'></ButtonText>
         </Link>
 
       </Form>
